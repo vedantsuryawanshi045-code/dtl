79e265d2f07d59960a73361942030f6880813b51
Vedant <vedantsuryawanshi045@gmail.com>
2026-04-16 20:00:37 +0530

    Add files via upload

---
+++ exp8.m
@@ -0,0 +1,33 @@
+% Matrix Operations Script
+
+clc;
+clear;
+
+% Define matrices
+A = [1 2; 3 4];
+B = [5 6; 7 8];
+
+% Matrix Addition
+C = A + B;
+
+% Matrix Subtraction
+D = A - B;
+
+% Matrix Multiplication
+E = A * B;
+
+% Display Results
+disp('Matrix A:')
+disp(A)
+
+disp('Matrix B:')
+disp(B)
+
+disp('Addition (A + B):')
+disp(C)
+
+disp('Subtraction (A - B):')
+disp(D)
+
+disp('Multiplication (A * B):')
+disp(E)
